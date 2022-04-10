aa6570d341aa516e62f7cee18bd3d00e1d031b6f
Roman Surinov <romasurinov13@mail.ru>
2022-04-10 14:10:29 +0300

    fix projects.json

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GitHubLink from './components/GitHubLink/GitHubLink';
 import Header from './components/Header/Header';
 import NavigationPanel from './components/NavigationPanel/NavigationPanel';
 import Project from './components/Project/Project';
+import PrjojectSection from './components/ProjectsSection/PrjojectSection';
 import Rain from './components/Rain/Rain';
 import SkillsSection from './components/SkillsSection/SkillsSection';
 import './index.css';
@@ -15,14 +16,15 @@ function App() {
     <section  >
       <a name="about"></a>
       <Header />
-      <Project
+      {/* <Project
         imageUrl={'https://aerospaceexport.com/wp-content/uploads/2019/12/project-management-I.jpg'}
         projectName={'Graph'}
         description={'Очень краткое описание'}
         stack={['One', 'Two', 'Three']}
         ghLink={'link'}
         previewLink={'link2'}
-      />
+      /> */}
+      <PrjojectSection />
       <NavigationPanel />
       <SkillsSection />
       <Rain />
--- src/components/ProjectsSection/PrjojectSection.js
+++ src/components/ProjectsSection/PrjojectSection.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Project from '../Project/Project';
 
-function PrjojectSections(props) {
+function PrjojectSection() {
 
-    const url = 'https://raw.githubusercontent.com/TheHarald/portfolio/master/info/skills.json'
+    const url = 'https://raw.githubusercontent.com/TheHarald/portfolio/master/info/projects.json'
 
     const [projects, setSProjects] = useState();
     const [isLoading, setIsLoading] = useState(true);
@@ -31,7 +31,10 @@ function PrjojectSections(props) {
             {isLoading ? <p>Loading</p> :
                 <div className='skills-section__cards'>
                     {projects.map((item, index) => {
+                        console.log(projects)
                         return <Project
+                            key={index}
+                            imageUrl={item.imageUrl}
                             projectName={item.projectName}
                             description={item.description}
                             stack={item.stack}
@@ -39,9 +42,17 @@ function PrjojectSections(props) {
                             previewLink={item.previewLink}
                         />
                     })}
+                    <Project
+                        imageUrl={'https://aerospaceexport.com/wp-content/uploads/2019/12/project-management-I.jpg'}
+                        projectName={'Graph'}
+                        description={'Очень краткое описание'}
+                        stack={['One', 'Two', 'Three']}
+                        ghLink={'link'}
+                        previewLink={'link2'}
+                    />
                 </div>}
         </div>
     );
 }
 
-export default PrjojectSections;
+export default PrjojectSection;
