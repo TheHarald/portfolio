c9bcc7359fc02e4e7afcc4b92c0dbf1b85bb7088
Roman Surinov <romasurinov13@mail.ru>
2022-04-22 23:50:43 +0300

    fix project page

--- src/pages/ProjectPage/ProjectPage.js
+++ src/pages/ProjectPage/ProjectPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GitHubLink from '../../components/GitHubLink/GitHubLink';
 import NeonIcon from '../../components/NeonIcon/NeonIcon';
 import Project from '../../components/Project/Project';
+import Tag from '../../components/Tag/Tag';
 import UsabilityCard from '../../components/UsabilityCard/UsabilityCard';
 import './projectPage.css'
+
 
 function ProjectPage(props) {
     return (
@@ -13,7 +16,53 @@ function ProjectPage(props) {
                 <h1 className='project-page__name'>Название проекта проекта</h1>
             </div>
 
-            <UsabilityCard />
+            <div className='project-page__main-info'>
+
+                <div className='project-page__text-block'>
+                    <p className='project-page__description'>Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает
+                        сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона,
+                        а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст..
+                        Здесь ваш текст.. Здесь ваш текст.."</p>
+                    <div className='project-page__block'>
+                        <Tag title={'Some'} />
+                        <Tag title={'Some'} />
+                        <Tag title={'Some'} />
+                        <Tag title={'Some'} />
+                        <Tag title={'Some'} />
+                        <Tag title={'Some'} />
+                        <Tag title={'Some'} />
+                    </div>
+                    <div className='project-page__block'>
+                        <UsabilityCard title={'Title'} description={'description'} />
+                        <UsabilityCard title={'Title'} description={'description'} />
+                        <UsabilityCard title={'Title'} description={'description'} />
+                        <UsabilityCard title={'Title'} description={'description'} />
+                    </div>
+
+                    <div className='project-page__block'>
+                        <div className='project-card__link-to-gh'>
+                            <GitHubLink url={'ghLink'} />
+                            <span className='project-card__link-title'>Исходный код</span>
+                        </div>
+                        <div className='project-card__link-to-gh'>
+                            <a href={'previewLink'}>
+                                <NeonIcon icon={'view_in_ar'} className={'project-card__preview'} />
+                            </a>
+                            <span className='project-card__link-title'>Превью проекта</span>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+                <img className='project-page__image' src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' />
+
+            </div>
+
+
+
+
 
         </div>
     );
